8706f0c2475f49684f8a5dfbe2146668fffb8335
Soong En Wong <wsoongen2015@gmail.com>
2025-07-04 19:19:36 +0100

    Initial Commit

---
+++ Unplug/UnplugApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnplugApp.swift
+//  Unplug
+//
+//  Created by Soong En Wong on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct UnplugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnplugTests/UnplugTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnplugTests.swift
+//  UnplugTests
+//
+//  Created by Soong En Wong on 04/07/2025.
+//
+
+import Testing
+@testable import Unplug
+
+struct UnplugTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
